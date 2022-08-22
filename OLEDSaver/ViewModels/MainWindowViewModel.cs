a6448674deffe4dc4c85c02a70317c6c13e463d7
Russell <Daedalus2051@users.noreply.github.com>
2022-08-21 23:47:22 -0400

    Moved backend code into VM

--- OLEDSaver/ViewModels/MainWindowViewModel.cs
+++ OLEDSaver/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using OLEDSaver.Models;
+using Prism.Commands;
 using Prism.Mvvm;
+using System;
+using System.CodeDom;
 using System.Windows;
 
 namespace OLEDSaver.ViewModels
@@ -7,17 +10,44 @@ namespace OLEDSaver.ViewModels
     public class MainWindowViewModel : BindableBase
     {
         WindowConfigSettingsModel windowConfigSettings;
+
+        private DelegateCommand _toggleWindowStateCommand;
+        public DelegateCommand ToggleWindowStateCommand { get { return _toggleWindowStateCommand; } }
+
+        private DelegateCommand _toggleWindowStyleCommand;
+        public DelegateCommand ToggleWindowStyleCommand { get { return _toggleWindowStyleCommand; } }
+
+        private DelegateCommand _toggleGUICommand;
+        public DelegateCommand ToggleGUICommand { get { return _toggleGUICommand; } }
+
+        private DelegateCommand _toggleBlackoutCommand;
+        public DelegateCommand ToggleBlackoutCommand { get { return _toggleBlackoutCommand; } }
+
+        private DelegateCommand _exitApplicationCommand;
+        public DelegateCommand ExitApplicationCommand { get { return _exitApplicationCommand; } }
+
         public MainWindowViewModel()
         {
             // property init
             windowConfigSettings = new WindowConfigSettingsModel();
 
             // command init
-
+            _exitApplicationCommand = new DelegateCommand(OnExitApplication);
+            _toggleWindowStateCommand = new DelegateCommand(ToggleWindowState);
+            _toggleWindowStyleCommand = new DelegateCommand(ToggleWindowStyle);
+            _toggleGUICommand = new DelegateCommand(ToggleGuiVisibility);
+            _toggleBlackoutCommand = new DelegateCommand(OnBlackout);
 
             // Everything else
             SetWindowDimensions();
+
+            // Defaults
+            DisplayGui = true;
+            WindowStyle = WindowStyle.SingleBorderWindow;
+            WindowState = WindowState.Normal;
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
         }
+
 
         #region Bindable Properties
 
@@ -33,14 +63,83 @@ namespace OLEDSaver.ViewModels
             get { return _windowHeight; }
             set { _windowHeight = value; RaisePropertyChanged(); }
         }
+        
+        private WindowStyle _windowStyle;        
+        public WindowStyle WindowStyle
+        {
+            get { return _windowStyle; }
+            set { _windowStyle = value; RaisePropertyChanged(); }
+        }
+        private WindowState _windowState;
+        public WindowState WindowState
+        {
+            get { return _windowState; }
+            set { _windowState = value; RaisePropertyChanged(); }
+        }
+        private WindowStartupLocation _windowStartupLocation;
+        public WindowStartupLocation WindowStartupLocation
+        {
+            get { return _windowStartupLocation; }
+            set { _windowStartupLocation = value; RaisePropertyChanged(); }
+        }
+
+        private bool _displayGui;
+        public bool DisplayGui
+        {
+            get { return _displayGui; }
+            set { _displayGui = value; RaisePropertyChanged(); }
+        }
 
         #endregion
 
         #region Methods
+
         private void SetWindowDimensions()
         {
             WindowWidth = windowConfigSettings.OriginalWidth;
             WindowHeight = windowConfigSettings.OriginalHeight;
+        }
+        private void OnBlackout()
+        {
+            ToggleWindowState();
+            ToggleWindowStyle();
+        }
+
+        private void OnExitApplication()
+        {
+            Application.Current.Shutdown();
+        }
+        /// <summary>
+        /// Toggles between the Visible and Hidden Visibility state for the <see cref="MainWindowGrid"/>.
+        /// </summary>
+        private void ToggleGuiVisibility()
+        {
+            if (DisplayGui)
+                DisplayGui = false;
+            else
+                DisplayGui = true;
+        }
+
+        /// <summary>
+        /// Toggles between the SingleBorderWindow and None WindowStyle for the <see cref="MainWindowObject"/>.
+        /// </summary>
+        private void ToggleWindowStyle()
+        {
+            if (WindowStyle == WindowStyle.SingleBorderWindow)
+                WindowStyle = WindowStyle.None;
+            else
+                WindowStyle = WindowStyle.SingleBorderWindow;
+        }
+
+        /// <summary>
+        /// Toggles between the Normal and Maximized WindowState for the <see cref="MainWindowObject"/>.
+        /// </summary>
+        private void ToggleWindowState()
+        {
+            if (WindowState == WindowState.Maximized)
+                WindowState = WindowState.Normal;
+            else
+                WindowState = WindowState.Maximized;
         }
         #endregion
     }
--- OLEDSaver/Views/MainWindow.xaml.cs
+++ OLEDSaver/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OLEDSaver.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,78 +25,6 @@ namespace OLEDSaver.Views
         public MainWindow()
         {
             InitializeComponent();
-
-            //_screenWidth = SystemParameters.PrimaryScreenWidth;
-            //_screenHeight = SystemParameters.PrimaryScreenHeight;
-
-            //_currentHeight = MainWindowObject.Height;
-            //_currentWidth = MainWindowObject.Width;
-
-            UpdateDisplayInfo();
-        }
-
-        private void Window_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.C) Application.Current.Shutdown();
-            if (e.Key == Key.F)
-            {
-                ToggleWindowState();
-                ToggleWindowStyle();
-            }
-            if (e.Key == Key.M)
-            {
-                ToggleWindowState();
-            }
-            if (e.Key == Key.S)
-            {
-                ToggleWindowStyle();
-            }
-            if (e.Key == Key.D)
-            {
-                ToggleGridVisibility();
-            }
-        }
-
-        /// <summary>
-        /// Toggles between the Visible and Hidden Visibility state for the <see cref="MainWindowGrid"/>.
-        /// </summary>
-        private void ToggleGridVisibility()
-        {
-            if (MainWindowGrid.Visibility == Visibility.Visible)
-                MainWindowGrid.Visibility = Visibility.Hidden;
-            else
-                MainWindowGrid.Visibility = Visibility.Visible;
-        }
-
-        /// <summary>
-        /// Toggles between the SingleBorderWindow and None WindowStyle for the <see cref="MainWindowObject"/>.
-        /// </summary>
-        private void ToggleWindowStyle()
-        {
-            if (MainWindowObject.WindowStyle == WindowStyle.SingleBorderWindow)
-                MainWindowObject.WindowStyle = WindowStyle.None;
-            else
-                MainWindowObject.WindowStyle = WindowStyle.SingleBorderWindow;
-        }
-
-        /// <summary>
-        /// Toggles between the Normal and Maximized WindowState for the <see cref="MainWindowObject"/>.
-        /// </summary>
-        private void ToggleWindowState()
-        {
-            if (MainWindowObject.WindowState == WindowState.Maximized)
-                MainWindowObject.WindowState = WindowState.Normal;
-            else
-                MainWindowObject.WindowState = WindowState.Maximized;
-        }
-
-        private void UpdateDisplayInfo()
-        {
-            //WindowHeightLabel.Content = $"Window Height: {_currentHeight}";
-            //WindowWidthLabel.Content = $"Window Width: {_currentWidth}";
-
-            //DisplayHeightLabel.Content = $"Display Height: {_screenHeight}";
-            //DisplayWidthLabel.Content = $"Display Width: {_screenWidth}";
         }
     }
 }
